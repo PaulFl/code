b031c7869614394c2133965b98cb264edf3846ba
Paul Fleury <paul.fleuryp@gmail.com>
2015-06-25 16:42:42 +0200

    u

--- python/remote.py
+++ python/remote.py
@@ -8,12 +8,27 @@ def callback(controlId, value):
 	print("value: ")
 	print(value)
 
+def XAxisCallback(value):
+	print("XValue: ", value)
+
+def YAxisCallbacl(value):
+	print("YValue: ", value)
+
 xboxCont = XboxController.XboxController(
-		controllerCallBack = callback,
+		controllerCallBack = None,
 		joystickNo = 0,
 		deadzone = 0.1,
 		scale = 1,
 		invertYAxis = False)
+
+xboxCont.setupControlCallback(
+		self.xboxCont.XboxControls.LTHUMBX,
+		XAxisCallback)
+
+xboxCont.setupControlCallback(
+		self.xboxCont.XboxControls.LTHUMBY,
+		YAxisCallback)
+
 
 
 xboxCont.start()
